91785ac26c26d8b843e63deac7a69ed0879dabad
Markus Kohlhase <markus.kohlhase@slowtec.de>
2022-07-12 19:01:36 +0200

    Use JsValue as return value for ThemeColors getter

--- src/theme.rs
+++ src/theme.rs
@@ -16,120 +16,119 @@ extern "C" {
     #[derive(Debug, Clone)]
     pub type ThemeColors;
 
-    // TODO all of these should accept an [R, G, B] array as well
     #[wasm_bindgen(method, getter)]
-    pub fn button_background_active(this: &ThemeColors) -> Option<String>;
+    pub fn button_background_active(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn button_background_hover(this: &ThemeColors) -> Option<String>;
+    pub fn button_background_hover(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn icons(this: &ThemeColors) -> Option<String>;
+    pub fn icons(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn icons_attention(this: &ThemeColors) -> Option<String>;
+    pub fn icons_attention(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn frame(this: &ThemeColors) -> Option<String>;
+    pub fn frame(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn frame_inactive(this: &ThemeColors) -> Option<String>;
+    pub fn frame_inactive(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn ntp_background(this: &ThemeColors) -> Option<String>;
+    pub fn ntp_background(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn ntp_text(this: &ThemeColors) -> Option<String>;
+    pub fn ntp_text(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn popup(this: &ThemeColors) -> Option<String>;
+    pub fn popup(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn popup_border(this: &ThemeColors) -> Option<String>;
+    pub fn popup_border(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn popup_highlight(this: &ThemeColors) -> Option<String>;
+    pub fn popup_highlight(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn popup_highlight_text(this: &ThemeColors) -> Option<String>;
+    pub fn popup_highlight_text(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn popup_text(this: &ThemeColors) -> Option<String>;
+    pub fn popup_text(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn sidebar(this: &ThemeColors) -> Option<String>;
+    pub fn sidebar(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn sidebar_border(this: &ThemeColors) -> Option<String>;
+    pub fn sidebar_border(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn sidebar_highlight(this: &ThemeColors) -> Option<String>;
+    pub fn sidebar_highlight(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn sidebar_highlight_text(this: &ThemeColors) -> Option<String>;
+    pub fn sidebar_highlight_text(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn sidebar_text(this: &ThemeColors) -> Option<String>;
+    pub fn sidebar_text(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn tab_background_separator(this: &ThemeColors) -> Option<String>;
+    pub fn tab_background_separator(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn tab_background_text(this: &ThemeColors) -> Option<String>;
+    pub fn tab_background_text(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn tab_line(this: &ThemeColors) -> Option<String>;
+    pub fn tab_line(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn tab_loading(this: &ThemeColors) -> Option<String>;
+    pub fn tab_loading(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn tab_selected(this: &ThemeColors) -> Option<String>;
+    pub fn tab_selected(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn tab_text(this: &ThemeColors) -> Option<String>;
+    pub fn tab_text(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn toolbar(this: &ThemeColors) -> Option<String>;
+    pub fn toolbar(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn toolbar_bottom_separator(this: &ThemeColors) -> Option<String>;
+    pub fn toolbar_bottom_separator(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn toolbar_field(this: &ThemeColors) -> Option<String>;
+    pub fn toolbar_field(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn toolbar_field_border(this: &ThemeColors) -> Option<String>;
+    pub fn toolbar_field_border(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn toolbar_field_border_focus(this: &ThemeColors) -> Option<String>;
+    pub fn toolbar_field_border_focus(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn toolbar_field_focus(this: &ThemeColors) -> Option<String>;
+    pub fn toolbar_field_focus(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn toolbar_field_highlight(this: &ThemeColors) -> Option<String>;
+    pub fn toolbar_field_highlight(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn toolbar_field_highlight_text(this: &ThemeColors) -> Option<String>;
+    pub fn toolbar_field_highlight_text(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn toolbar_field_separator(this: &ThemeColors) -> Option<String>;
+    pub fn toolbar_field_separator(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn toolbar_field_text(this: &ThemeColors) -> Option<String>;
+    pub fn toolbar_field_text(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn toolbar_field_text_focus(this: &ThemeColors) -> Option<String>;
+    pub fn toolbar_field_text_focus(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn toolbar_text(this: &ThemeColors) -> Option<String>;
+    pub fn toolbar_text(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn toolbar_top_separator(this: &ThemeColors) -> Option<String>;
+    pub fn toolbar_top_separator(this: &ThemeColors) -> JsValue;
 
     #[wasm_bindgen(method, getter)]
-    pub fn toolbar_vertical_separator(this: &ThemeColors) -> Option<String>;
+    pub fn toolbar_vertical_separator(this: &ThemeColors) -> JsValue;
 }
 
 #[wasm_bindgen]
@@ -137,11 +136,9 @@ extern "C" {
     #[derive(Debug, Clone)]
     pub type ThemeProperties;
 
-    // TODO have this return a Vec of enum ?
     #[wasm_bindgen(method, getter)]
     pub fn additional_backgrounds_alignment(this: &ThemeProperties) -> Option<Array>;
 
-    // TODO have this return a Vec of enum ?
     #[wasm_bindgen(method, getter)]
     pub fn additional_backgrounds_tiling(this: &ThemeProperties) -> Option<Array>;
 }
